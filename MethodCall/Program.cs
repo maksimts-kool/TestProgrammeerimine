be878214f6c2c66089f451320fc954ee3d939cf0
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-03 16:33:29 +0200

    Create

---
+++ MethodCall/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method");
+            //kuidas kutsuda esile Main1 meetod siin:
+            Main1();
+        }
+        static void Main1()
+        {
+            Console.WriteLine("Main1 method");
+        }
+    }
+}
