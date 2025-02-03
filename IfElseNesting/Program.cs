21e03c5ad801e7f7d7b4a1d838c5cefaa878a210
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-03 14:50:17 +0200

    Update Program.cs

--- IfElseNesting/Program.cs
+++ IfElseNesting/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Number Control!");
+            int number = 189;
+
+            //Kontroll
+            if (number == 189)
+            {
+                Console.WriteLine("Number is bigger");
+
+                if (number > 190 && number < 200)
+                {
+                    Console.WriteLine("Number is between 190 to 200");
+                }
+                else
+                {
+                    Console.WriteLine("Number is bigger than 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is smaller");
+
+            }
         }
     }
 }
