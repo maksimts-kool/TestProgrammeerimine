9490c31ddb707b4a9884f1e39307a2c4d61e36d0
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-03 15:34:43 +0200

    Create

---
+++ IfElseColor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IfElseColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siseta värv");
+            string värv = Console.ReadLine();
+            if (värv == "red" || värv == "blue" || värv == "green" || värv == "white")
+            {
+                Console.WriteLine("You chose " + värv);
+            }
+            else 
+            {
+                Console.WriteLine("Color not recognised");
+            }
+        }
+    }
+}
