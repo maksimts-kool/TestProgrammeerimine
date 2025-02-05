9ce44f78ca464bf5dd095a034aa05a9c044f8a75
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-05 14:53:52 +0200

    Create

---
+++ CoinCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CoinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raadius = 6378160000;
+            double umbermoood = 2 * Math.PI * raadius;
+            double mundiLaebimood = 2.5;
+            double mundidKogus = umbermoood / mundiLaebimood;
+
+            Console.WriteLine("Maa ümbermõõt on " + Math.Round(umbermoood) + " cm.");
+            Console.WriteLine("Kahe eurost münti on vaja: " + Math.Round(mundidKogus));
+        }
+    }
+}
