bba56b33a42843999fdb532d42a47f37658a3f26
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-05 15:20:01 +0200

    Create

---
+++ SwitchVocal/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwitchVocal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siseta täht ja vajuta enter");
+            string ch = Console.ReadLine();
+            switch (ch)
+            {
+                case "a":
+                    Console.WriteLine("Vokaal a");
+                    break;
+                case "e":
+                    Console.WriteLine("Vokaal e");
+                    break;
+                case "i":
+                    Console.WriteLine("Vokaal i");
+                    break;
+                default:
+                    Console.WriteLine("Ei ole vokaal e täishäälik");
+                    break;
+
+            }
+        }
+    }
+}
