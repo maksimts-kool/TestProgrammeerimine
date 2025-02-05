ac906376076d6c1a3f8379dcbcaf34a1571908b3
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-05 16:32:06 +0200

    Create

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber = new Random().Next(1, 7);
+
+            Console.WriteLine(randomNumber);
+
+            switch (randomNumber)
+            {
+                case 1:
+                    Console.WriteLine("number on 1");
+                    break;
+                case 2:
+                    Console.WriteLine("number on 2");
+                    break;
+                case 3:
+                    Console.WriteLine("number on 3");
+                    break;
+                case 4:
+                    Console.WriteLine("number on 4");
+                    break;
+                case 5:
+                    Console.WriteLine("number on 5");
+                    break;
+                case 6:
+                    Console.WriteLine("number on 6");
+                    break;
+                default:
+                    Console.WriteLine("Viga number");
+                    break;
+            }
+        }
+    }
+}
