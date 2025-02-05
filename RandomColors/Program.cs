5fc8eaef6f6ee48ecbb06a75cdc37fe02fc0e758
Opilane TTHK <maksimtsitkool@gmail.com>
2025-02-05 16:03:27 +0200

    Update Program.cs

--- RandomColors/Program.cs
+++ RandomColors/Program.cs
@@ -4,31 +4,43 @@ class Program
 {
     static void Main()
     {
-        Color c = (Color)(new Random()).Next(0, 5);
-        switch (c)
+       
+        
+        for (int i=0;i<1000;i++)
+        {
+            Color c = (Color)(new Random()).Next(0, 5);
+            switch (c)
         {
             case Color.Red:
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Red");
+                    Console.Beep();
                 break;
             case Color.Green:
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Green");
-                break;
+                    Console.Beep();
+                    break;
             case Color.Blue:
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Blue");
-                break;
+                    Console.Beep();
+                    break;
             case Color.White:
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("White");
-                break;
+                    Console.Beep();
+                    break;
             case Color.Orange:
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("Orange");
-                break;
+                    Console.Beep();
+                    break;
+                    default:
+                    break;
         }
         Console.ResetColor();
+    }
     }
     public enum Color
     { 
