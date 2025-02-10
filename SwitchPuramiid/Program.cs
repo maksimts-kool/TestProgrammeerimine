9f50a3f0dd1d8154ddc8bd137b413584e2c2b09c
maksimts-kool <maksimtsitkool@gmail.com>
2025-02-10 14:07:44 +0200

    Create

---
+++ SwitchPuramiid/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SwitchPuramiid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siseta puramiidi suurus");
+            int i, j, n;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= n-i; j++)
+                { 
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
