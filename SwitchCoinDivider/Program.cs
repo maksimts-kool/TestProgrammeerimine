0f5a84c4bfa93c9096bc2648df2b3f3e10b2e804
maksimts-kool <maksimtsitkool@gmail.com>
2025-02-10 13:47:00 +0200

    Create

---
+++ SwitchCoinDivider/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SwitchCoinDivider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much coins:");
+            int coins = int.Parse(Console.ReadLine());
+            int five = coins / 50;
+            coins -= five * 50;
+            int two = coins / 20;
+            coins -= two * 20;
+            int ten = coins / 10;
+            coins -= ten * 10;
+            int five0 = coins / 5;
+            coins -= five0 * 5;
+            int two0 = coins / 2;
+            coins -= two0 * 2;
+            int one = coins;
+            Console.WriteLine("You have: ");
+            if (five > 0)
+            {
+                Console.WriteLine($"{five} - 50 cent coins");
+            }
+            else if (two > 0)
+            {
+                Console.WriteLine($"{two} - 20 cent coins");
+            }
+            else if (ten > 0)
+            {
+                Console.WriteLine($"{ten} - 10 cent coins");
+            }
+            else if (five0 > 0)
+            {
+                Console.WriteLine($"{five0} - 5 cent coins");
+            }
+            else if (two0 > 0)
+            {
+                Console.WriteLine($"{two0} - 2 cent coins");
+            }
+            else if (one > 0)
+            {
+                Console.WriteLine($"{one} - 1 cent coins");
+            }
+        }
+    }
+}
